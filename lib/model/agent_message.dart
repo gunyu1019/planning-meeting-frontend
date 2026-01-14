3d42c89516080a56c4449927348ad6ca764fa30d
gunyu1019 <gunyu1019@gmail.com>
2026-01-14 18:14:04 +0900

    [Feat] Implement AgentMessage model.

---
+++ lib/model/agent_message.dart
@@ -0,0 +1,16 @@
+
+
+class AgentMessage {
+  final String id;
+  final String role;
+
+  /// This Unique-ID uses to keep session.
+  final String threadId;
+  final String tenantId;
+
+  final DateTime created;
+
+  final String content;
+
+  AgentMessage(this.id, this.role, this.threadId, this.tenantId, this.created, this.content);
+}
