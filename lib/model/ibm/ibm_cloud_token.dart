854574f827f3091b341b1a8715fb34bff2beddef
gunyu1019 <gunyu1019@gmail.com>
2026-01-14 18:13:40 +0900

    [Feat] Implement IBMAuthentic Model.

---
+++ lib/model/ibm/ibm_cloud_token.dart
@@ -0,0 +1,29 @@
+
+class IBMAuthentic {
+  final String accessToken;
+  final String refreshToken;
+  final int imsUserId;
+  final String tokenType;
+  final int expiresIn;
+  final DateTime expiration;
+  final List<String> scope;
+
+  IBMAuthentic._(this.accessToken, this.refreshToken, this.imsUserId, this.tokenType, this.expiresIn, this.expiration, this.scope);
+
+  @override
+  String toString() {
+    return '$tokenType $accessToken';
+  }
+
+  factory IBMAuthentic.fromJson(Map<String, dynamic> json) {
+    return IBMAuthentic._(
+      json['access_token'],
+      json['refresh_token'],
+      json['ims_user_id'],
+      json['token_type'],
+      json['expires_in'],
+      DateTime.fromMillisecondsSinceEpoch(json['expiration']),
+      json['scope'].toString().split(" "),
+    );
+  }
+}
